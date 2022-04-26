f14e50d826e86d90c365a764c201d50130511be0
IversonPaulino <iversonoliveira1999@live.com>
2022-04-25 22:43:11 -0300

    Comit push

---
+++ src/TEDs/ForIfElsePack/ForIfElse.java
@@ -0,0 +1,23 @@
+//FAZER COMPLEMENTOS GET&SET DO MAIN...
+package TEDs.ForIfElsePack;
+
+public class ForIfElse {
+    private double altura;
+    private char sexo;
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+}
